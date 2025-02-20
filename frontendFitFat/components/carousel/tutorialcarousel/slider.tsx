8385bb6d4e7c0b5423f46f9dcda942608a41cc39
Cipher0023 <dani.esparza@hotmail.com>
2025-02-20 19:19:06 -0300

    tutorial carousel finished, we still have some bugs to iron out

--- frontendFitFat/components/carousel/tutorialcarousel/slider.tsx
+++ frontendFitFat/components/carousel/tutorialcarousel/slider.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet, Text, View, FlatList, ViewToken, Platform, Dimensions} from "react-native";
-import React, { useState, useRef, useMemo} from "react";
+import React, { useState, useRef, useMemo, useEffect} from "react";
 import { ImageSlider,ImageSliderType } from "@/placeholderData/data";
 import SliderItem from "../../carousel/tutorialcarousel/sliderItem";
-import Animated,{ useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
+import Animated,{ useAnimatedScrollHandler, useSharedValue, useAnimatedRef, useDerivedValue, scrollTo} from "react-native-reanimated";
 import Pagination from "./pagination";
 
 
@@ -12,11 +12,17 @@ type props ={
 
 const { width } = Dimensions.get('window');
 
+
 const Slider = ({ itemList }: props) => {
 
   const scrollX = useSharedValue(0);
   const [paginationIndex, setPaginationIndex] = useState(0);
   const [data, setData] = useState(itemList);
+  const itemListRef = useRef(itemList);
+  const ref = useAnimatedRef<Animated.FlatList<any>>();
+  const [isAutoPlay, setIsAutoPlay] = useState(true);
+  const interval = useRef<NodeJS.Timeout>();
+  const offset = useSharedValue(0)
 
   
   const onsScrollHandler = useAnimatedScrollHandler({
@@ -27,29 +33,54 @@ const Slider = ({ itemList }: props) => {
         const newIndex = Math.round(e.contentOffset.x / width);
         setPaginationIndex(newIndex % itemList.length);
       }
-    }
+    },
+    onMomentumEnd: (e) => {
+      offset.value = e.contentOffset.x;
+    },
   });
+
+  useEffect(() => {
+    itemListRef.current = itemList;
+    if (isAutoPlay == true) {
+      interval.current = setInterval(() => {
+        offset.value = offset.value + width
+      }, 3000);
+
+    } else {
+      clearInterval(interval.current);
+    }
+
+    return () => {
+      clearInterval(interval.current);
+    }
+  }, [itemList, isAutoPlay, offset, width]);
+
+  useDerivedValue(() => {
+    scrollTo(ref, offset.value, 0, true);
+
+  })
 
   const viewabilityConfig = {
     itemVisiblePercentThreshold: 50
   }
 
-const viewabilityConfigCallbackPairs = useMemo(() => {
-  if (Platform.OS === 'web') return [];
-  
-  return [{
-    viewabilityConfig: { itemVisiblePercentThreshold: 50 },
-    onViewableItemsChanged: ({ viewableItems }: { viewableItems: ViewToken[] }) => {
-      if (viewableItems[0]?.index != null) {
-        setPaginationIndex(viewableItems[0].index);
+  const viewabilityConfigCallbackPairs = useMemo(() => {
+    if (Platform.OS === 'web') return [];
+
+    return [{
+      viewabilityConfig: { itemVisiblePercentThreshold: 50 },
+      onViewableItemsChanged: ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+        if (viewableItems[0]?.index != null) {
+          setPaginationIndex(viewableItems[0].index % itemListRef.current.length);
+        }
       }
-    }
-  }];
-}, []); // Dependências vazias = referência estável
+    }];
+  }, []); // Dependências vazias = referência estável
 
   return (
     <View>
       <Animated.FlatList
+        ref={ref}
         data={data}
         keyExtractor={(item, index) => `sliderItem-${index}`}
         renderItem={({item, index})=> (
@@ -63,6 +94,12 @@ const viewabilityConfigCallbackPairs = useMemo(() => {
         viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs}
         onEndReached={()=> setData([...data, ...itemList])}
         onEndReachedThreshold={0.5}
+        onScrollBeginDrag={() => {
+          setIsAutoPlay(false);
+        }}
+        onScrollEndDrag={() => {
+          setIsAutoPlay(true);
+        }}
       />
       <Pagination itens={itemList} paginationIndex={paginationIndex} scrollX={scrollX} />
 
@@ -76,3 +113,4 @@ export default Slider
 const styles= StyleSheet.create({
 });
 
+//28:53
