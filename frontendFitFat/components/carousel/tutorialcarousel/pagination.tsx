72ecc741a92b512b4b9220d69f96628d2bf6f4f1
Cipher0023 <dani.esparza@hotmail.com>
2025-02-19 22:33:39 -0300

    a carousel indicator placed and almost finished

--- frontendFitFat/components/carousel/tutorialcarousel/pagination.tsx
+++ frontendFitFat/components/carousel/tutorialcarousel/pagination.tsx
@@ -1,25 +1,39 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Dimensions} from 'react-native'
 import React from 'react'
 import { ImageSliderType } from '@/placeholderData/data'
-import { SharedValue } from 'react-native-reanimated';
+import Animated,{ SharedValue, useAnimatedStyle, interpolate, Extrapolation} from 'react-native-reanimated';
 
 type Props = {
   itens: ImageSliderType[];
   paginationIndex: number;
   scrollX: SharedValue<number>;
-
 }
+
+const{width} =Dimensions.get('window');
 
 const Pagination = ({itens,paginationIndex,scrollX}:Props) => {
   return (
 
     <View style={styles.container}>
       {itens.map((_,index)=>{
+        const pgAnimationStyle = useAnimatedStyle(()=>{
+          const dotWidth = interpolate(
+            scrollX.value,
+            [(index - 1) * width, index * width, (index + 1) * width],
+            [8, 20, 8],
+            Extrapolation.CLAMP
+          );
+          return {
+            width: dotWidth
+          };
+        });
+
         return (
-          <View 
+          <Animated.View 
             key={index} 
             style={[
-              styles.dot, 
+              styles.dot,
+              pgAnimationStyle,
               {backgroundColor: paginationIndex === index ? '#222': '#aaa'},
             ]}
           />
--- frontendFitFat/components/carousel/tutorialcarousel/slider.tsx
+++ frontendFitFat/components/carousel/tutorialcarousel/slider.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, FlatList, ViewToken} from "react-native";
-import React, { useState, useRef } from "react";
+import { StyleSheet, Text, View, FlatList, ViewToken, Platform, Dimensions} from "react-native";
+import React, { useState, useRef, useMemo} from "react";
 import { ImageSlider,ImageSliderType } from "@/placeholderData/data";
 import SliderItem from "../../carousel/tutorialcarousel/sliderItem";
 import Animated,{ useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
@@ -10,35 +10,47 @@ type props ={
   itemList: ImageSliderType[]
 }
 
+const { width } = Dimensions.get('window');
+
 const Slider = ({ itemList }: props) => {
 
   const scrollX = useSharedValue(0);
   const [paginationIndex, setPaginationIndex] = useState(0);
+  const [data, setData] = useState(itemList);
 
+  
   const onsScrollHandler = useAnimatedScrollHandler({
     onScroll: (e) => {
       scrollX.value = e.contentOffset.x;
-
+      
+      if (Platform.OS === 'web') {
+        const newIndex = Math.round(e.contentOffset.x / width);
+        setPaginationIndex(newIndex % itemList.length);
+      }
     }
-  })
+  });
 
-  const onViewableItemsChanged = ({viewableItems}: {viewableItems: ViewToken[]}) => {
-    if(viewableItems[0].index !== undefined && viewableItems[0].index !== null)
-      setPaginationIndex(viewableItems[0].index);
-  };
-  
   const viewabilityConfig = {
     itemVisiblePercentThreshold: 50
   }
 
-  const viewabilityConfigCallbackPairs = useRef([
-    {viewabilityConfig, onViewableItemsChanged}
-  ]);
+const viewabilityConfigCallbackPairs = useMemo(() => {
+  if (Platform.OS === 'web') return [];
+  
+  return [{
+    viewabilityConfig: { itemVisiblePercentThreshold: 50 },
+    onViewableItemsChanged: ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems[0]?.index != null) {
+        setPaginationIndex(viewableItems[0].index);
+      }
+    }
+  }];
+}, []); // Dependências vazias = referência estável
 
   return (
     <View>
       <Animated.FlatList
-        data={itemList}
+        data={data}
         keyExtractor={(item, index) => `sliderItem-${index}`}
         renderItem={({item, index})=> (
           <SliderItem item={item} index={index} scrollX={scrollX}/>
@@ -48,7 +60,9 @@ const Slider = ({ itemList }: props) => {
         pagingEnabled
         onScroll={onsScrollHandler}
         removeClippedSubviews={false} //if this is enabled the other items will not be rendered
-        viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
+        viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs}
+        onEndReached={()=> setData([...data, ...itemList])}
+        onEndReachedThreshold={0.5}
       />
       <Pagination itens={itemList} paginationIndex={paginationIndex} scrollX={scrollX} />
 
